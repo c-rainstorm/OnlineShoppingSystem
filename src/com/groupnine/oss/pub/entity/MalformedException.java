c74c3124b4252bd167321968b8570c22cada4390
Robert Darwin <robert.darwin@outlook.com>
2016-11-26 17:28:04 +0800

    MalformedException: 增加后端统一使用的异常类

---
+++ src/com/groupnine/oss/pub/entity/MalformedException.java
@@ -0,0 +1,24 @@
+package com.groupnine.oss.pub.entity;
+
+public class MalformedException extends Exception {
+
+    // Constructors
+
+    public MalformedException() {
+        super();
+    }
+
+    public MalformedException(String message) {
+        super(message);
+    }
+
+    public MalformedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MalformedException(Throwable cause) {
+        super(cause);
+    }
+
+    // No new methods
+}
